21ab943068dbb51f8b1caaa186f1f818757db306
Trevor Blanarik <tblanarik@gmail.com>
2017-02-14 21:11:53 -0800

    pylint yelled at me and i changed things until it stopped

--- src/fb.py
+++ src/fb.py
@@ -1,27 +1,40 @@
+"""
+Loads the stored linear regressions, pulls the latest travel time,
+saves the prediction as times.json
+
+@author Trevor Blanarik
+"""
+
 import json
 import datetime
 
-lynncsv = r'/home/tblanarik/wsdot/data/Lynnwood_to_Downtown_Seattle.csv'
-last_line = open(lynncsv).readlines()[-1]
-last_data = last_line.strip().split(',')
-curtime = int(last_data[1])
-dt = datetime.datetime.strptime(last_data[-1], "%Y%m%d_%H%M")
+def predict():
+    """
+    Loads the stored linear regressions, pulls the latest travel time,
+    saves the prediction as times.json
+    """
+    lynncsv = r'/home/tblanarik/wsdot/data/Lynnwood_to_Downtown_Seattle.csv'
+    last_line = open(lynncsv).readlines()[-1]
+    last_data = last_line.strip().split(',')
+    curtime = int(last_data[1])
+    mdt = datetime.datetime.strptime(last_data[-1], "%Y%m%d_%H%M")
+    is_weekday = 0 if mdt.isoweekday() < 5 else 1
 
-is_weekday = 0 if dt.isoweekday() < 5 else 1
+    regressions_1_hour = json.load(open(r'/home/tblanarik/wsdot3/regressions_1_hour.json'))
 
-regressions_1_hour = json.load(open(r'/home/tblanarik/wsdot3/regressions_1_hour.json'))
+    linreg = regressions_1_hour[str(is_weekday)][str(mdt.hour)][str(mdt.minute)]
 
-linreg = regressions_1_hour[str(is_weekday)][str(dt.hour)][str(dt.minute)]
+    predict = linreg['slope']*curtime + linreg['intercept']
 
-predict = linreg['slope']*curtime + linreg['intercept']
+    prev_data = json.load(open(r'/home/tblanarik/mysite/data/times.json'))
 
-prev_data = json.load(open(r'/home/tblanarik/mysite/data/times.json'))
+    data = {"LynnwoodToSeattleCurrent": curtime,
+            "LynnwoodToSeattlePrediction": int(predict),
+            "LynnwoodToSeattlePreviousPrediction":int(prev_data["LynnwoodToSeattlePrediction"]),
+            "LastUpdated": mdt.strftime("%H:%M")}
 
-data = {"LynnwoodToSeattleCurrent": curtime,
-        "LynnwoodToSeattlePrediction": int(predict),
-        "LynnwoodToSeattlePreviousPrediction":int(prev_data["LynnwoodToSeattlePrediction"]),
-        "LastUpdated": dt.strftime("%H:%M")}
+    outfile = open(r'/home/tblanarik/mysite/data/times.json', 'w')
+    json.dump(data, outfile)
+    outfile.close()
 
-outfile = open(r'/home/tblanarik/mysite/data/times.json', 'w')
-json.dump(data, outfile)
-outfile.close()
+predict()
